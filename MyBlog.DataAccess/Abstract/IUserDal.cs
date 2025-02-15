23caeff9c8634cfcfc74cfc9b24e7f74bf7989c7
Samet Çınar <samet.cnr96@hotmail.com>
2025-02-15 22:29:18 +0300

    Upd

--- MyBlog.DataAccess/Abstract/IUserDal.cs
+++
@@ -1,9 +0,0 @@
-﻿using MyBlog.Entities.Identity;
-
-namespace MyBlog.DataAccess.Abstract
-{
-    public interface IUserDal : IEntityRepository<ApplicationUser>
-    {
-        // Kullanıcıya özel veri erişim metotlarını burada tanımlayabilirsin.
-    }
-}
